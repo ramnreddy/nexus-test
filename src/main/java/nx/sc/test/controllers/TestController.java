cc8c144811c6e8ce6f68d38f9b85614a0d4211b5
ramnreddy <reddymnr@yahoo.com>
2021-06-05 23:42:45 +0800

    initial commit

---
+++ src/main/java/nx/sc/test/controllers/TestController.java
@@ -0,0 +1,7 @@
+package nx.sc.test.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+}
---
+++ src/main/java/nx/sc/test/service/TestService.java
@@ -0,0 +1,7 @@
+package nx.sc.test.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestService {
+}
